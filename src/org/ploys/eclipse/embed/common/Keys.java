6d31f93844648e4db5144567fdf15e87c4d0dfb8
sharky <nik.sharky@gmail.com>
2015-05-06 12:04:29 +0300

    Structural refactor

--- src/org/ploys/eclipse/embed/common/Keys.java
+++
@@ -1,5 +0,0 @@
-package org.ploys.eclipse.embed.common;
-
-public class Keys {
-	public static final String PLUGIN_ID = "org.ploys.eclipse.embed";
-}
--- src/org/ploys/eclipse/embed/terminal/ui/PortSendHandler.java
+++
@@ -1,5 +0,0 @@
-package org.ploys.eclipse.embed.terminal.ui;
-
-public interface PortSendHandler {
-	void sendData(byte[] data, boolean hdup);
-}
